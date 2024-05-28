cc89035ec561394430520d9a61db4aaec43d20da
Aioxx <120001849+aioxx@users.noreply.github.com>
2024-05-28 20:55:58 +0600

    Update route.js

--- app/quotes/random/route.js
+++ app/quotes/random/route.js
@@ -3,7 +3,7 @@ import data from 'data/quotes.json';
 
 export const dynamic = 'force-dynamic'; // Otherwise, Next.js will cache this handler's output
 
-const dataSource = 'https://en.wikipedia.org/wiki/AFI%27s_100_Years...100_Movie_Quotes';
+const dataSource = 'https://randomuser.me/api/?nat=us';
 
 export async function GET() {
     const randomId = Math.floor(Math.random() * data.length);
